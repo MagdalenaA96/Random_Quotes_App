d3415b96d09b5cc274e102dc60dc6a77a256668c
MagdalenaA96 <kowalikmagdalena1996@gmail.com>
2025-02-02 16:49:43 +0100

    implement basic server with GET /api/quotes endpoint

---
+++ server.js
@@ -0,0 +1,30 @@
+const http = require("http");
+
+const {
+  getQuotes,
+  getQuote,
+  getRandomQuote,
+} = require("./controllers/quoteControler");
+const { log } = require("console");
+
+const PORT = 8080;
+const API_CONTENT_TYPE = { "Content-Type": "application/json" };
+
+const server = http.createServer(async function (req, res) {
+  console.log("Request");
+
+  if (req.url === "/api/quotes" && req.method === "GET") {
+    let quotes = await getQuotes();
+
+    if (quotes) {
+      res.writeHead(200, API_CONTENT_TYPE);
+    } else {
+      res.writeHead(404, API_CONTENT_TYPE);
+      quotes = { message: "Quotes not found" };
+    }
+
+    res.end(JSON.stringify(quotes));
+  }
+});
+
+server.listen(PORT);
